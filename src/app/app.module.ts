f4197a21b9e949be227ca106d01458e10649251f
belenfurtado <belenfurtadocoria@gmail.com>
2024-10-21 02:31:02 +0100

    new version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'; // Import this module
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'; // Import this module
 
 import { AppComponent } from './app.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
@@ -8,19 +8,12 @@ import { ScorePanelComponent } from './score-panel/score-panel.component';
 import { OptionsPanelComponent } from './options-panel/options-panel.component';
 import { TimerComponent } from './timer/timer.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    PokemonComponent,
-    ScorePanelComponent,
-    OptionsPanelComponent,
-    TimerComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule // Add HttpClientModule here
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        PokemonComponent,
+        ScorePanelComponent,
+        OptionsPanelComponent,
+        TimerComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
